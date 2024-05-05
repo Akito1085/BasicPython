5075d8bda3960d95e2774f68d0fde68f2e03086d
Akito1085 <a.oishi.24.4.1402@4nonome.com>
2024-05-05 23:56:41 +0900

    Python入門

--- pi.py
+++ pi.py
@@ -1,7 +1,15 @@
+# TODO
+#問 2. 文字列
 text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
+    How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
 # TODO
+
+a=text.strip()
+b=a.replace(",","")
+c=b.replace(".","")
+print(list(map(len,c.split(" "))))
+#314159265358979323846264
+##
 
