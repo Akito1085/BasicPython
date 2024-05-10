726c9fe0b899927a4cde7eea4a53495dbb863489
Akito1085 <a.oishi.24.4.1402@4nonome.com>
2024-05-10 14:17:32 +0900

    制御構文課題①,②

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,18 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+
+from math import pi
+a=sin(0)
+b=sin(pi/2)
+N=100
+h=b-a/N
+s=0 #面積s
+for k in range(1,N+1):
+    print(k)
+    s_k=h/2*((a+(k-1)*h)+(a+k*h)) #s_K=K番目の時の面積
+    s=s+s_k
+print(s)
+
+
+
