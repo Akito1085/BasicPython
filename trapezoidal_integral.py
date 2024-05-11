ab48bfbe1fbd5d00508cc8c5f5d8585b27fc1f87
Akito1085 <a.oishi.24.4.1402@4nonome.com>
2024-05-11 15:38:23 +0900

    修正

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -5,14 +5,13 @@ from math import sin
 # -----------
 
 from math import pi
-a=sin(0)
-b=sin(pi/2)
+a=0 #区間a
+b=pi/2 #区間b
 N=100
-h=b-a/N
+h=(b-a)/N
 s=0 #面積s
 for k in range(1,N+1):
-    print(k)
-    s_k=h/2*((a+(k-1)*h)+(a+k*h)) #s_K=K番目の時の面積
+    s_k=h/2*(sin(a+(k-1)*h)+sin(a+k*h)) #s_K=K番目の時の面積
     s=s+s_k
 print(s)
 
