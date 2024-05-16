18221ee0bc3bb63c591c9f5b8e4b77c733c35465
Akito1085 <a.oishi.24.4.1402@4nonome.com>
2024-05-16 14:10:47 +0900

    関数③④

--- euclid.py
+++ euclid.py
@@ -1,24 +1,31 @@
 a_list=[10,14,91]
 b_list=[20,91,14]
 
-for a,b in zip(a_list,b_list):
+a=int(input("enter"))
+b=int(input("number"))
 
+def euclid(a,b):
     if a>b:
         while True:
             if a%b==0:
                 break
             else:
                 a,b=b,a%b
-        print(b)
+        return b
     elif a==b:
-        print(a)
+        return a
     else:
         while True:
             if b%a==0:
                 break
             else:
                 b,a=a,b%a
-        print(a)
-    
-# TODO
-#aとbの最大公約数　a=bq+r r=a%b q=a//b , bの値をaに代入、a%bをbに代入するのを繰り返す。rが0になった時にやめる#
+        return a
+print("問1の答え",euclid(a,b)) 
+
+#問2
+def coprime(a,b):
+    return euclid(a,b)==1
+
+print("問2の答え",coprime(a,b))
+
